ca97f3dd66a9bc42df64cf316f1f387dfe38b6d3
Sebastian Adolfsson <sadolfsson@hotmail.com>
2023-03-16 18:31:01 +0100

    Person model added

---
+++ Database/Models/Person.cs
@@ -0,0 +1,15 @@
+﻿namespace Database.Models
+{
+	internal class Person
+	{
+		public int Id { get; set; }
+		public string FirstName { get; set; } = null!;
+		public string LastName { get; set; } = null!;
+		public string Email { get; set; } = null!;
+		public string? PhoneNumber { get; set; }
+		public string Title { get; set; } = null!;
+		public string Description { get; set; } = null!;
+		public DateTime CreatedAt { get; set; }
+		public string StatusName { get; set; } = null!;
+	}
+}
