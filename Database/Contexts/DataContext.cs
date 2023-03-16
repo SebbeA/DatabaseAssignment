1069f26a65a7438e638b3b09d21946ddf963ae5d
Sebastian Adolfsson <sadolfsson@hotmail.com>
2023-03-16 18:14:00 +0100

    DataContext added

---
+++ Database/Contexts/DataContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Database.Contexts
+{
+	internal class DataContext : DbContext
+	{
+		private readonly string _connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\sadol\OneDrive\Skrivbord\WIN22\5.-Datalagring\Database_CA\Database\Contexts\sql_db.mdf;Integrated Security=True;Connect Timeout=30";
+
+		#region constructors
+
+		public DataContext()
+		{
+
+		}
+		public DataContext(DbContextOptions<DataContext> options) : base(options)
+		{
+		}
+
+		#endregion
+
+		#region overrides
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			if (!optionsBuilder.IsConfigured)
+				optionsBuilder.UseSqlServer(_connectionString);
+		}
+
+		#endregion
+	}
+}
