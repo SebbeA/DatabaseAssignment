fd7be79a08dae3382613513c16777155fdc48669
Sebastian Adolfsson <sadolfsson@hotmail.com>
2023-03-19 14:19:28 +0100

    MenuService added

--- Database/Program.cs
+++ Database/Program.cs
@@ -1,1 +1,51 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Database.Services;
+
+var menu = new MenuService();
+
+while (true)
+{
+	Console.Clear();
+	Console.Write("Välkommen! Vänligen ett av följande alternativ (1-6): \n");
+	Console.WriteLine("1. Skapa ett ärende ");
+	Console.WriteLine("2. Visa alla ärenden ");
+	Console.WriteLine("3. Visa ett specifik ärende ");
+	Console.WriteLine("4. Uppdatera ett specifikt ärende ");
+	Console.WriteLine("5. Radera ett specifikt ärende ");
+	Console.WriteLine("6. Avsluta. \n");
+
+
+	switch (Console.ReadLine())
+	{
+		case "1":
+			Console.Clear();
+			await menu.CreateNewIssueAsync();
+			break;
+
+		case "2":
+			Console.Clear();
+			await menu.ShowAllIssuesAsync();
+			break;
+
+		case "3":
+			Console.Clear();
+			await menu.ShowOneIssueAsync();
+			break;
+
+		case "4":
+			Console.Clear();
+			await menu.UpdateIssueAsync();
+			break;
+
+		case "5":
+			Console.Clear();
+			await menu.DeleteIssueAsync();
+			break;
+
+		case "6":
+			Environment.Exit(0);
+			break;
+	}
+
+	Console.WriteLine("\nTryck på valfri knapp för att fortsätta...");
+	Console.ReadKey();
+}
---
+++ Database/Services/MenuService.cs
@@ -0,0 +1,181 @@
+﻿using Database.Models;
+
+namespace Database.Services
+{
+	internal class MenuService
+	{
+		public async Task CreateNewIssueAsync()
+		{
+			var person = new Person();
+
+			Console.Write("Ange förnamn: ");
+			person.FirstName = Console.ReadLine() ?? "";
+
+			Console.Write("Ange efternamn: ");
+			person.LastName = Console.ReadLine() ?? "";
+
+			Console.Write("Ange email: ");
+			person.Email = Console.ReadLine() ?? "";
+
+			Console.Write("Ange telefonnummer: ");
+			person.PhoneNumber = Console.ReadLine() ?? "";
+			Console.Write("");
+
+			Console.Write("Ange titel på ditt problem: ");
+			person.Title = Console.ReadLine() ?? "";
+
+			Console.Write("Ange en beskrivning: ");
+			person.Description = Console.ReadLine() ?? "";
+
+			Console.Write("");
+
+			Console.Write("Ange någon av statusarna: ");
+			Console.Write("Ej påbörjad ");
+			Console.Write("Pågående ");
+			Console.Write("Avslutad \n");
+			person.StatusName = Console.ReadLine() ?? "";
+
+			person.CreatedAt = DateTime.Now;
+
+			// save issue to database
+			await PersonService.SaveAsync(person);
+		}
+		public async Task ShowAllIssuesAsync()
+		{
+			// get all customers + addresses from database
+			var persons = await PersonService.GetAllAsync();
+
+			if (persons.Any())
+			{
+				foreach (Person person in persons)
+				{
+					Console.WriteLine($"Kundnummer: {person.Id}");
+					Console.WriteLine($"Namn: {person.FirstName} {person.LastName}");
+					Console.WriteLine($"E-postadress: {person.Email}");
+					Console.WriteLine($"Telefonnummer: {person.PhoneNumber}");
+					Console.WriteLine("");
+					Console.WriteLine($"Titel på problemet: {person.Title} ");
+					Console.WriteLine($"Beskrivning på problemet: {person.Description} ");
+					Console.WriteLine($"Problem skapat: {person.CreatedAt} ");
+					Console.WriteLine("");
+					Console.WriteLine($"Status på ärendet: {person.StatusName}");
+					Console.WriteLine("");
+				}
+			}
+			else
+			{
+				Console.WriteLine("Inga problem finns i databasen.");
+				Console.WriteLine("");
+			}
+
+		}
+		public async Task ShowOneIssueAsync()
+		{
+
+			Console.Write("Ange e-postadress på kunden: ");
+			var email = Console.ReadLine();
+
+			if (!string.IsNullOrEmpty(email))
+			{
+				// get specific issue + status from database
+				var person = await PersonService.GetAsync(email);
+
+				if (person != null)
+				{
+					Console.WriteLine($"Kundnummer: {person.Id}");
+					Console.WriteLine($"Namn: {person.FirstName} {person.LastName}");
+					Console.WriteLine($"E-postadress: {person.Email}");
+					Console.WriteLine($"Telefonnummer: {person.PhoneNumber}");
+					Console.WriteLine("");
+					Console.WriteLine($"Titel på problemet: {person.Title} ");
+					Console.WriteLine($"Beskrivning på problemet: {person.Description} ");
+					Console.WriteLine($"Problem skapat: {person.CreatedAt} ");
+					Console.WriteLine("");
+					Console.WriteLine($"Status på ärendet: {person.StatusName}");
+					Console.WriteLine("");
+				}
+				else
+				{
+					Console.Clear();
+					Console.WriteLine($"Ingen kund med den angivna e-postadresses {email} hittades.");
+					Console.WriteLine("");
+				}
+			}
+			else
+			{
+				Console.WriteLine("Ingen e-postadress angiven.");
+				Console.WriteLine("");
+			}
+		}
+		public async Task UpdateIssueAsync()
+		{
+			Console.Write("Ange e-postadress på kunden: ");
+			var email = Console.ReadLine();
+
+			if (!string.IsNullOrEmpty(email))
+			{
+				var person = await PersonService.GetAsync(email);
+				if (person != null)
+				{
+					Console.WriteLine("Skriv in nya information på de fält som du vill uppdatera. \n");
+
+					Console.Write("Ange förnamn: ");
+					person.FirstName = Console.ReadLine() ?? "";
+
+					Console.Write("Ange efternamn: ");
+					person.LastName = Console.ReadLine() ?? "";
+
+					Console.Write("Ange email: ");
+					person.Email = Console.ReadLine() ?? "";
+
+					Console.Write("Ange telefonnummer: ");
+					person.PhoneNumber = Console.ReadLine() ?? "";
+
+					Console.Write("Ange ny titel: ");
+					person.Title = Console.ReadLine() ?? "";
+
+					Console.Write("Ange ny beskrivning: ");
+					person.Description = Console.ReadLine() ?? "";
+
+					Console.Write("Ange den nya statusen: ");
+					Console.Write("Ej påbörjad ");
+					Console.Write("Pågående ");
+					Console.Write("Avslutad \n");
+					person.StatusName = Console.ReadLine() ?? "";
+
+					person.CreatedAt = DateTime.Now;
+
+					// update issue and status from database
+					await PersonService.UpdateAsync(person);
+				}
+				else
+				{
+					Console.WriteLine("Ingen kund med hittades med den angivna e-postadressen.");
+					Console.WriteLine("");
+				}
+			}
+			else
+			{
+				Console.WriteLine("Ingen e-postadress angiven.");
+				Console.WriteLine("");
+			}
+		}
+		public async Task DeleteIssueAsync()
+		{
+
+			Console.Write("Ange e-postadress på kunden: ");
+			var email = Console.ReadLine();
+
+			if (!string.IsNullOrEmpty(email))
+			{
+				// delete customer from database
+				await PersonService.DeleteAsync(email);
+			}
+			else
+			{
+				Console.WriteLine("Ingen e-postadress angiven.");
+				Console.WriteLine("");
+			}
+		}
+	}
+}
